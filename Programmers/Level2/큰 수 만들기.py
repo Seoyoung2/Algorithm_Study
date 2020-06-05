f6f1dfd8833b2951457424454bab6e111862f2b0
seoyoung <ww0111@naver.com>
2020-06-05 18:42:51 +0900

    fix 큰수 만들기

--- Programmers/Level2/큰 수 만들기.py
+++ Programmers/Level2/큰 수 만들기.py
@@ -1,23 +1,26 @@
 # Greedy (https://programmers.co.kr/learn/courses/30/lessons/42883)
+# 짱 어려ㅜ움,,,,,,,,,,,,,,,,,,,,,,ㅠㅠ
 
 
 def solution(number, k):
-    tmp = list(number)
-    idx = tmp.index(max(tmp[:k]))
-    tmp = tmp[idx:]
-    cnt = 0
-    i = 0
-    while True:
-        if tmp[i] < tmp[i+1]:
-            tmp.remove(str(tmp[i]))
-            cnt += 1
-        if cnt == k-idx:
+    length = len(number)
+    m = 0
+    for cnt in range(k):
+        for idx in range(m, length-1):
+            if number[idx] < number[idx+1]:
+                number = number[:idx] + number[idx+1:]
+                length -= 1
+                if idx > 0:
+                    m = idx-1
+                break
+        else:
+            number = number[:length-k+cnt]
             break
-        i += 1
-    return "".join(tmp)
+    return "".join([str(i) for i in number])
 
 
 print(solution("1924", 2))              #"94"
 print(solution("1231234", 3))           #"3234"
 print(solution("4177252841", 4))        #"775841"
 print(solution("98769876", 3))        #"99876"
+print(solution("55555", 3))        #"55"
