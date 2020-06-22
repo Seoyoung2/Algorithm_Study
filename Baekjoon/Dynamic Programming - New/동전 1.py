623551fdce5d60c037c18e817ccc5c029271a4ba
seoyoung <ww0111@naver.com>
2020-06-22 13:48:35 +0900

    BJ: 동전1 solve

--- Baekjoon/Dynamic Programming - New/동전 1.py
+++ Baekjoon/Dynamic Programming - New/동전 1.py
@@ -3,3 +3,11 @@
 from sys import stdin
 
 n, k = map(int, stdin.readline().split())
+coin = [0] * n
+for i in range(n):
+    coin[i] = int(stdin.readline())
+dp = [1] + [0] * k
+for i in range(n):
+    for j in range(coin[i], k+1):
+        dp[j] += dp[j-coin[i]]
+print(dp[k])
