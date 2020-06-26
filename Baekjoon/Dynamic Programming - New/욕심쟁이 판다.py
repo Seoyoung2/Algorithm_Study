c2b5ecc5728456e354da40bae725ec6f68187d5d
seoyoung <ww0111@naver.com>
2020-06-26 23:12:24 +0900

    BJ: 욕심많은 판다

---
+++ Baekjoon/Dynamic Programming - New/욕심쟁이 판다.py
@@ -0,0 +1,27 @@
+# https://www.acmicpc.net/problem/1937
+# DFS + DP
+
+from sys import stdin
+
+n = int(stdin.readline())
+M = [list(map(int, stdin.readline().split())) for _ in range(n)]
+dp = [[0] * n for _ in range(n)]
+
+
+def dfs(x, y):
+    m = 0
+    if dp[x][y] == 0:
+        dp[x][y] = 1
+        for dx, dy in (1, 0), (-1, 0), (0, 1), (0, -1):
+            if 0 <= x+dx < n and 0 <= y+dy < n:
+                if M[x][y] < M[x+dx][y+dy]:
+                    m = max(m, bfs(x+dx, y+dy))
+        dp[x][y] += m
+    return dp[x][y]
+
+
+for i in range(n):
+    for j in range(n):
+        if not dp[i][j]:
+            bfs(i, j)
+print(max([max(num) for num in dp]))
