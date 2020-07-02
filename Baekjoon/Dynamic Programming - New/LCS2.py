a8dd72cb28732a923c32324786aec0e90d0b2558
seoyoung <ww0111@naver.com>
2020-07-02 12:22:20 +0900

    BJ: LCS2(DP) solve

--- Baekjoon/Dynamic Programming - New/LCS2.py
+++ Baekjoon/Dynamic Programming - New/LCS2.py
@@ -1,19 +1,27 @@
 # https://www.acmicpc.net/problem/9252
+# dp배열에 길이만 넣은 후, 다시 배열을 순회하며 문자열을 출력하였음
+# --> 처음부터 dp배열에 문자열을 대입하는 방법 있음
 
 from sys import stdin
 
 A = " "+stdin.readline().rstrip()
 B = " "+stdin.readline().rstrip()
+
 dp = [[0] * len(B) for _ in range(len(A))]
-ans = []
 for i in range(1, len(A)):
     for j in range(1, len(B)):
         if A[i] == B[j]:
             dp[i][j] = dp[i-1][j-1] + 1
         else:
             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+res = dp[-1][-1]
+print(res)
 
-for i in range(1, len(A)):
-    for j in range(1, len(B)):
-        if dp[i][j] != dp[i][j-1] and dp[i][j] != dp[i-1][j]:
-            print(A[i], B[j])
+ans = []
+for i in range(len(A)-1, 0, -1):
+    for j in range(len(B)-1, 0, -1):
+        if dp[i][j] == res and dp[i][j] != dp[i][j-1] and dp[i][j] != dp[i-1][j]:
+            res -= 1
+            ans.append(A[i])
+            break
+print("".join(reversed(ans)))
