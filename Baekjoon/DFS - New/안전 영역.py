a9f8278d2744646c717c72e29957aa5a44cf757b
seoyoung <ww0111@naver.com>
2020-07-07 17:54:05 +0900

    BJ: 안전영역 fix

--- Baekjoon/DFS - New/안전 영역.py
+++ Baekjoon/DFS - New/안전 영역.py
@@ -1,5 +1,6 @@
 # https://www.acmicpc.net/problem/2468
-# BFS로 풀었을 때보다 시간이 너무 오래걸림(DFS)
+# DFS로 재귀함수를 호출하는 방식으로 풀었더니 시간이 너무 오래 걸림
+# ==> stack을 이용해서 재귀호출 없이 DFS로 풀었더니 훨씬 ㄱㅊ
 
 from sys import stdin
 
@@ -7,12 +8,15 @@ n = int(stdin.readline())
 L = [list(map(int, stdin.readline().split())) for _ in range(n)]
 
 
-def dfs(x, y, height):
-    visit[x][y] = 1
-    for dx, dy in (1, 0), (-1, 0), (0, 1), (0, -1):
-        if 0 <= x+dx < n and 0 <= y+dy < n:
-            if L[x+dx][y+dy] > height and not visit[x+dx][y+dy]:
-                dfs(x+dx, y+dy, height)
+def dfs(cx, cy, height):
+    stack = [(cx, cy)]
+    while stack:
+        x, y = stack.pop()
+        for dx, dy in (1, 0), (-1, 0), (0, 1), (0, -1):
+            if 0 <= x+dx < n and 0 <= y+dy < n:
+                if L[x+dx][y+dy] > height and not visit[x+dx][y+dy]:
+                    visit[x+dx][y+dy] = 1
+                    stack.append((x+dx, y+dy))
 
 
 ans = 0
@@ -24,6 +28,7 @@ for h in range(0, high):
         for j in range(n):
             if L[i][j] > h and not visit[i][j]:
                 cnt += 1
+                visit[i][j] = 1
                 dfs(i, j, h)
     ans = max(ans, cnt)
 print(ans)
