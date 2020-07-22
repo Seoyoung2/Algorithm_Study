94e905f71d72d0090bcaef3a57d0e848cb11542e
seoyoung <ww0111@naver.com>
2020-07-22 16:44:06 +0900

    BJ: Floyd-Warshall

---
+++ Baekjoon/Floyd-Warshall/바이러스.py
@@ -0,0 +1,24 @@
+# https://www.acmicpc.net/problem/2606
+
+from sys import stdin
+
+v = int(stdin.readline())
+e = int(stdin.readline())
+graph = [[100] * (v+1) for _ in range(v+1)]
+for _ in range(e):
+    a, b = map(int, stdin.readline().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
+
+for k in range(1, v+1):
+    for i in range(1, v+1):
+        for j in range(1, v+1):
+            if i == j:
+                graph[i][j] = 0
+            else:
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+ans = 0
+for num in graph[1]:
+    if 0 < num < 100:
+        ans += 1
+print(ans)
--- Baekjoon/기타/ACM 호텔.py
+++ Baekjoon/기타/ACM 호텔.py
@@ -1,10 +1,18 @@
 # https://www.acmicpc.net/problem/10250
 
 from sys import stdin
+from math import ceil
 
 T = int(stdin.readline())
 for _ in range(T):
     h, w, n = map(int, stdin.readline().split())
     flo = str(n % h) if n % h != 0 else str(h)
-    ho = str(1 + n // h) if n % h != 0 else str(h)
+    ho = str(ceil(n / h))
     print("".join(flo + "0" + ho)) if len(ho) == 1 else print("".join(flo + ho))
+
+# 6 12 10 => 402
+# 30 50 72 => 1203
+# 1 1 1 => 101
+# 6 5 6 => 601
+# 6 5 12 => 602
+# 6 5 10 => 402
