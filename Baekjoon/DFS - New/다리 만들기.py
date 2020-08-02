e61d21c1896c2ab0fae517dfa8b8f7bf6569056b
seoyoung <ww0111@naver.com>
2020-08-02 17:13:08 +0900

    BJ: 다리만들기 success

--- Baekjoon/DFS - New/다리 만들기.py
+++ Baekjoon/DFS - New/다리 만들기.py
@@ -1,14 +1,15 @@
 # https://www.acmicpc.net/problem/2146
 # 어려웠던 문제,,
 # 각 섬마다 번호를 붙인다 -> 섬인 구역부터 dfs를 수행해서 다른 번호가 붙은 섬이 나타나면 종료 -> 가장 짧은 경로 구한다
-# 위 방법으로는 시간초과가 발생 ==> 섬 전체를 dfs를 돌지 말고, 섬 외곽 지역만 dfs 수행?
+# 위 방법으로는 시간초과가 발생 ==> 섬 전체를 dfs를 돌지 말고, 섬 외곽 지역만 dfs 수행 (이 방법 역시 시간초과 발생..)
+# ==> dfs대신 bfs로 풀었더니 이버에는 메모리초과.. ==> dist라는 배열로 방문체크했더니 성공 !!
 
 from sys import stdin
+from collections import deque
 
 N = int(stdin.readline())
 L = [list(map(int, stdin.readline().split())) for _ in range(N)]
 ans = 100000
-visit = [L[k][:] for k in range(N)]
 
 
 # 섬마다 번호 붙이기
@@ -23,21 +24,26 @@ def assign_num(x, y, n):
                     stack.append((cx+dx, cy+dy))
 
 
-def dfs(x, y, cnt, n):
+def get_distance(qn):
     global ans
-    if cnt > ans:
-        return
-    for dx, dy in (1, 0), (-1, 0), (0, 1), (0, -1):
-        if 0 <= x+dx < N and 0 <= y+dy < N:
-            if L[x+dx][y+dy] != n:
-                if L[x+dx][y+dy] == 0:
-                    L[x+dx][y+dy] = n
-                    dfs(x+dx, y+dy, cnt+1, n)
-                    L[x+dx][y+dy] = 0
-                else:
-                    if ans > cnt:
-                        ans = cnt
+    dist = [[-1] * N for _ in range(N)]
+    q = deque()
+    for m in range(N):
+        for n in range(N):
+            if L[m][n] == qn:
+                q.append((m, n))
+                dist[m][n] = 0
+
+    while q:
+        x, y = q.popleft()
+        for dx, dy in (1, 0), (-1, 0), (0, 1), (0, -1):
+            if 0 <= x+dx < N and 0 <= y+dy < N:
+                if L[x+dx][y+dy] and L[x+dx][y+dy] != qn:
+                    ans = min(ans, dist[x][y])
                     return
+                if not L[x+dx][y+dy] and dist[x+dx][y+dy] == -1:
+                    dist[x+dx][y+dy] = dist[x][y] + 1
+                    q.append((x+dx, y+dy))
 
 
 # 섬마다 번호 붙이기 (2부터)
@@ -49,12 +55,6 @@ for i in range(N):
             assign_num(i, j, num)
 
 # 가장 짧은 다리의 길이 찾기
-for i in range(N):
-    for j in range(N):
-        num = L[i][j]
-        if num > 0:
-            dfs(i, j, 0, num)
-
+for k in range(2, num+1):
+    get_distance(k)
 print(ans)
-for row in L:
-    print(row)
